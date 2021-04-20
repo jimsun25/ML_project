ae5721984f50d4ebea236e6ef4b6be112a485f51
jimsun25 <60873997+jimsun25@users.noreply.github.com>
2021-04-20 23:36:53 +0800

    Add files via upload

---
+++ 04_Clustering/Hierarchical Clustering/hc_do.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 19:22:24 2019
+
+@author: jimsu
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:,3:5].values
+
+# Building the dendrogram
+import scipy.cluster as sch 
+dendrogram = sch.hierarchy.dendrogram(sch.hierarchy.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Applying hierarchical clustering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+# Visualizing the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Careful')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Standard')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Target')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Sensible')
+plt.title('Clusters of clients')
+plt.xlabel('Annual income (ks)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
